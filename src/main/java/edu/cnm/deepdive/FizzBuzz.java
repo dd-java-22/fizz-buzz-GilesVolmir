02a16da882e8f6a0bc0e21e8bac13134d54b8e88
Skylar <sscott28@cnm.edu>
2026-01-28 14:57:58 -0700

    Implemented and passing with accumulating into a string.

--- src/main/java/edu/cnm/deepdive/FizzBuzz.java
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -3,20 +3,22 @@ package edu.cnm.deepdive;
 public class FizzBuzz {
 
   public String fizzBuzz(int position) {
+    String result = "";
     boolean factorOf3 = position % 3 == 0;
     boolean factorOf5 = position % 5 == 0;
-    if (factorOf3 && factorOf5) {
-      return "FizzBuzz";
-    } else if (factorOf3) {
-      return "Fizz";
-    } else if (factorOf5) {
-      return "Buzz";
-    } else {
-      return Integer.toString(position);
+    if (factorOf3) {
+      result += "Fizz";
     }
+    if (factorOf5) {
+      result += "Buzz";
+    }
+    if (result.isEmpty()) {
+      result = Integer.toString(position);
+    }
+    return result;
   }
 
-  public static void main(String[] args) {
+  static void main(String[] args) {
     int max = 100;
     int parsedArg;
     if (args.length > 0 && (parsedArg = Integer.parseInt(args[0])) > 0) {
