9501265d9390a0eda14459572867be978deb5c23
Skylar <sscott28@cnm.edu>
2026-01-28 16:40:27 -0700

    Added Map to perform string choosing behavior in main instead of string of "if"s.

--- src/main/java/edu/cnm/deepdive/Main.java
+++ src/main/java/edu/cnm/deepdive/Main.java
@@ -1,8 +1,16 @@
 package edu.cnm.deepdive;
 
+import java.util.EnumSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
+
+  private static final Map<Set<FizzBuzz>, String> FIZZBUZZ_MAP = Map.of(
+      EnumSet.of(FizzBuzz.FIZZ), "Fizz",
+      EnumSet.of(FizzBuzz.BUZZ), "Buzz",
+      EnumSet.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), "FizzBuzz"
+      );
 
   static void main(String[] args) {
     int max = 100;
@@ -11,17 +19,10 @@ public class Main {
       max = parsedArg;
     }
     for (int i = 1; i <= max; i++) {
-      Set<FizzBuzz> result = (FizzBuzz.fizzBuzz(i));
-      if (result.contains(FizzBuzz.FIZZ)) {
-        System.out.print("Fizz");
-      }
-      if (result.contains(FizzBuzz.BUZZ)) {
-        System.out.print("Buzz");
-      }
-      if (result.isEmpty()) {
-        System.out.print(i);
-      }
-      System.out.println();
+      Set<FizzBuzz> result = FizzBuzz.fizzBuzz(i);
+      System.out.println(
+          FIZZBUZZ_MAP.getOrDefault(result, Integer.toString(i))
+      );
     }
   }
 }
